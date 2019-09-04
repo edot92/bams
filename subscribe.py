1170db94acce7053999d3c68dd13f0f050b736f0
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-04 19:18:03 +0700

    modifikasi filter data-data sensor

--- subscribe.py
+++ subscribe.py
@@ -18,19 +18,17 @@ def on_message(client, userdata, msg):
     node = msg.payload[0:3].decode('ascii')
     timestamp = int(msg.payload[3:11], 16)
 
-    # Hanya node kode sb2 yang memiliki data akselerometer axial ketiga sebanyak 100 data.
-    # Node sb2 ada 300 data, selain itu 200 data akselerometer saja.
-    if node == 'sb2':
-        total_akselerometer = len(msg.payload[11:311])
-        print(msg.payload[311:])
-    else:
-        total_akselerometer = len(msg.payload[11:211])
-        print(msg.payload[211:])
+    sensor = [struct.unpack('!f', bytes.fromhex(msg.payload[i:i+n].decode('ascii')))[0]
+           for i in range(11, len(msg.payload[11:]) + n, n)]
 
-    acc = [struct.unpack('!f', bytes.fromhex(msg.payload[i:i+n].decode('ascii')))[0]
-           for i in range(11, total_akselerometer, n)]
+    if node == "sb1" or node == "sb2":
+        acc = sensor[:-3]
+        ane1 = sensor[-1]
+        ane2 = sensor[-2]
+        ane3 = sensor[-3]
 
-    print(node, timestamp, acc[0], acc[1], '...\n')
+    print(node)
+    print(sensor, '\n')
 
 
 if __name__ == "__main__":
@@ -43,7 +41,7 @@ if __name__ == "__main__":
     client.connect("bbta3.bppt.go.id", 9621)
 
     try:
-        client.username_pw_set('bams', 'bams.pwd')
+        client.username_pw_set(os.getenv('MQ_USER'), os.getenv('MQ_PWD'))
     except KeyError:
         print('Silahkan tambah env. untuk MQ_USER dan/atau MQ_PWD')
         sys.exit()
