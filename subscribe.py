b46deb263d46ab1f1a39311e25e410c988995f28
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-04 10:36:04 +0700

    koreksi pemilahan data akselerometer

--- subscribe.py
+++ subscribe.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import struct
 import paho.mqtt.client as mqtt
 
@@ -16,10 +17,20 @@ def on_message(client, userdata, msg):
     n = 8  # split every 8 characters
     node = msg.payload[0:3].decode('ascii')
     timestamp = int(msg.payload[3:11], 16)
-    acc = [struct.unpack('!f', bytes.fromhex(msg.payload[i:i+n].decode('ascii')))[0]
-           for i in range(11, len(msg.payload[11:]), n)]
 
-    print(node, timestamp, acc[0], acc[1], '...')
+    # Hanya node kode sb2 yang memiliki data akselerometer axial ketiga sebanyak 100 data.
+    # Node sb2 ada 300 data, selain itu 200 data akselerometer saja.
+    if node == 'sb2':
+        total_akselerometer = len(msg.payload[11:311])
+        print(msg.payload[311:])
+    else:
+        total_akselerometer = len(msg.payload[11:211])
+        print(msg.payload[211:])
+
+    acc = [struct.unpack('!f', bytes.fromhex(msg.payload[i:i+n].decode('ascii')))[0]
+           for i in range(11, total_akselerometer, n)]
+
+    print(node, timestamp, acc[0], acc[1], '...\n')
 
 
 if __name__ == "__main__":
@@ -29,10 +40,11 @@ if __name__ == "__main__":
 
     print("Connecting to broker...")
 
-    if not os.environ['MQ_USER'] or not os.environ['MQ_PWD']:
-        print('Silahkan tambah env. untuk MQ_USER dan/atau MQ_PWD')
-
     client.connect("bbta3.bppt.go.id", 9621)
-    client.username_pw_set(os.environ['MQ_USER'], os.environ['MQ_PWD'])
+    try:
+        client.username_pw_set(os.environ['MQ_USER'], os.environ['MQ_PWD'])
+    except KeyError:
+        print('Silahkan tambah env. untuk MQ_USER dan/atau MQ_PWD')
+        sys.exit()
 
     client.loop_forever()
