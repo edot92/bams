4afe0ce0cc3fe0393781641c7ce5db7169466d28
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-06 11:13:34 +0700

    ambil data-data sensor yang diperlukan dari binary

--- subscribe.js
+++ subscribe.js
@@ -5,13 +5,21 @@ const client = MQTT.connect("mqtt://bbta3.bppt.go.id:9621", {
     password: process.env.MQ_PWD
 })
 
+const n = 8  // pisah setiap 8 karakter
 const onMessage = async (topic) => {
     try {
         await client.subscribe("BAMS")
         if (topic.payload) {
             const node = topic.payload.toString().slice(0, 3)
             const timestamp = parseInt(topic.payload.toString().slice(3, 11), 16) * 1000
-            console.log(node, timestamp)
+            const raw_sensor = topic.payload.toString().slice(11, topic.payload.toString().length)
+            let sensor = []
+
+            for (let index = 11; index <= raw_sensor.length + n; index += n) {
+                sensor.push(Buffer.from(topic.payload.toString().slice(index, index + n),'hex').readFloatBE(0))
+            }
+
+            console.log(node, timestamp, sensor.length, '\n')
         }
     } catch (error) {
         console.log(error)
