eea2c7c881ef1666c6642e8da702399ac9b9da44
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-11 15:40:21 +0700

    membuat halaman dummy

---
+++ ui/src/pages/Data.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page padding>
+    HALAMAN DATA
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: "HalamanIndex",
+  data() {
+    return {};
+  },
+  created() {},
+  methods: {}
+};
+</script>
