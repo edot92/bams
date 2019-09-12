c75489723b321b8721cce64f35d424ab3d0c8b75
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-12 11:10:29 +0700

    update min width electron window

--- ui/src-electron/main-process/electron-main.js
+++ ui/src-electron/main-process/electron-main.js
@@ -15,7 +15,7 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    width: 1000,
+    minWidth: 1500,
     height: 600,
     useContentSize: true,
     webPreferences: {
