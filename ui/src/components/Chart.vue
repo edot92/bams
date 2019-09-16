042caed07aee933f421ab178a55d9119b2e80211
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-16 15:45:09 +0700

    penambahan properti untuk komponen chart

--- ui/src/components/Chart.vue
+++ ui/src/components/Chart.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div v-if="loading">
+    <div class="q-mb-md" v-if="loading">
       Sedang memuat...
       <q-spinner-cube color="primary" size="2em" />
     </div>
@@ -9,25 +9,28 @@
 </template>
 
 <script>
-import Chart from "chart.js";
+import Chart from "chart.js"
 
 export default {
   name: "ChartComponent",
-  props: ["connection"],
+  props: ["connection", "nodeSensor"],
   data() {
     return {
       client: this.connection,
+      node_sensor: this.nodeSensor,
       loading: true,
       maxData: 500,
       mChart: null,
-    };
+    }
   },
   mounted() {
-    this.streamData();
+    if (this.node_sensor == null) this.node_sensor = "sb1"
+
+    this.streamData()
   },
   methods: {
     drawTheChart() {
-      let ctx = document.getElementById("streamChart").getContext("2d");
+      let ctx = document.getElementById("streamChart").getContext("2d")
       this.mChart = new Chart(ctx, {
         type: "line",
         data: {
@@ -87,73 +90,76 @@ export default {
             ]
           }
         }
-      });
+      })
     },
     streamData() {
-      const n = 8; // pisah setiap 8 karakter
-      let waktu_ms = 0; // satuan milli second
+      const n = 8 // pisah setiap 8 karakter
+      let waktu_ms = 0 // satuan milli second
 
       this.client.on("connect", () => {
-        this.drawTheChart();
-      });
+        this.drawTheChart()
+      })
 
       this.client.on("packetreceive", async topic => {
         try {
-          await this.client.subscribe("BAMS");
+          await this.client.subscribe("BAMS")
           if (topic.payload) {
-            this.loading = false;
-            const node = topic.payload.toString().slice(0, 3);
+            this.loading = false
+            const node = topic.payload.toString().slice(0, 3)
             const timestamp =
-              parseInt(topic.payload.toString().slice(3, 11), 16) * 1000;
+              parseInt(topic.payload.toString().slice(3, 11), 16) * 1000
             const raw_sensor = topic.payload
               .toString()
-              .slice(11, topic.payload.toString().length);
+              .slice(11, topic.payload.toString().length)
 
-            let acc1 = [];
-            let acc2 = [];
-            let acc3 = [];
-            let ane1 = null;
-            let ane2 = null;
-            let ane3 = null;
+            let acc1 = []
+            let acc2 = []
+            let acc3 = []
+            let ane1 = null
+            let ane2 = null
+            let ane3 = null
 
             for (let index = 11; index <= raw_sensor.length + n; index += n) {
               const sensor_item_raw = topic.payload
                 .toString()
-                .slice(index, index + n);
+                .slice(index, index + n)
               const sensor_item = Buffer.from(
                 sensor_item_raw,
                 "hex"
-              ).readFloatBE(0);
+              ).readFloatBE(0)
 
-              if (node == "sb2") {
+              if (this.node_sensor == "sb2") {
+                console.log("sb2")
                 if (index >= 11 && index <= 803) {
-                  acc1.push(sensor_item);
+                  acc1.push(sensor_item)
                 } else if (index >= 811 && index <= 1603) {
-                  acc2.push(sensor_item);
+                  acc2.push(sensor_item)
                 } else if (index >= 1611 && index <= 2403) {
-                  acc3.push(sensor_item);
+                  acc3.push(sensor_item)
                 } else if (index == 2411) {
-                  ane1 = sensor_item;
+                  ane1 = sensor_item
                 } else if (index == 2419) {
-                  ane2 = sensor_item;
+                  ane2 = sensor_item
                 } else if (index == 2427) {
-                  ane3 = sensor_item;
+                  ane3 = sensor_item
                 }
-              } else if (node == "sb1") {
+              } else if (this.node_sensor == "sb1") {
+                console.log("sb1")
                 if (index >= 11 && index <= 803) {
-                  acc1.push(sensor_item);
+                  acc1.push(sensor_item)
                 } else if (index >= 811 && index <= 1603) {
-                  acc2.push(sensor_item);
+                  acc2.push(sensor_item)
                 } else if (index == 1611) {
-                  ane1 = sensor_item;
+                  ane1 = sensor_item
                 } else if (index == 1619) {
-                  ane2 = sensor_item;
+                  ane2 = sensor_item
                 } else if (index == 1627) {
-                  ane3 = sensor_item;
+                  ane3 = sensor_item
                 }
               } else {
+                console.log("sb3")
                 if (index >= 11 && index <= 803) {
-                  acc1.push(sensor_item);
+                  acc1.push(sensor_item)
                 } else if (index >= 811 && index <= 1603) {
                   acc2.push(sensor_item);
                 }
@@ -162,35 +168,35 @@ export default {
 
             for (let i = 0; i < 100; i++) {
               if (this.mChart.data.labels.length > this.maxData)
-                this.mChart.data.labels.shift();
-              let waktu_lokal = new Date(timestamp + waktu_ms);
-              this.mChart.data.labels.push(waktu_lokal.toLocaleString());
+                this.mChart.data.labels.shift()
+              let waktu_lokal = new Date(timestamp + waktu_ms)
+              this.mChart.data.labels.push(waktu_lokal.toLocaleTimeString())
 
               this.mChart.data.datasets.forEach(dataset => {
                 if (dataset.data.length > this.maxData) {
-                  dataset.data.shift();
+                  dataset.data.shift()
                 }
 
                 if (dataset.label == "Akselerometer 1") {
-                  dataset.data.push(acc1[i]);
+                  dataset.data.push(acc1[i])
                 } else {
-                  dataset.data.push(acc2[i]);
+                  dataset.data.push(acc2[i])
                 }
-              });
+              })
 
-              this.mChart.update();
+              this.mChart.update()
 
-              waktu_ms += 10; // dalam satuan ms, 1 s ada 100 data getaran
+              waktu_ms += 10 // dalam satuan ms, 1 s ada 100 data getaran
             }
           }
         } catch (error) {
-          console.log(error);
-          this.loading = true;
+          console.log(error)
+          this.loading = true
         }
-      });
+      })
     }
   }
-};
+}
 </script>
 
 <style>
