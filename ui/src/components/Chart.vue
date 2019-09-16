a8b2501db7ebc0289574de2dc1ddb427b4c6df6d
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-16 14:09:42 +0700

    membuat komponen awal untuk chart

---
+++ ui/src/components/Chart.vue
@@ -0,0 +1,197 @@
+<template>
+  <div>
+    <div v-if="loading">
+      Sedang memuat...
+      <q-spinner-cube color="primary" size="2em" />
+    </div>
+    <canvas id="streamChart" class="full-width" style="max-height: 580px;"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from "chart.js";
+
+export default {
+  name: "ChartComponent",
+  props: ["connection"],
+  data() {
+    return {
+      client: this.connection,
+      loading: true,
+      maxData: 500,
+      mChart: null,
+    };
+  },
+  mounted() {
+    this.streamData();
+  },
+  methods: {
+    drawTheChart() {
+      let ctx = document.getElementById("streamChart").getContext("2d");
+      this.mChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: [],
+          datasets: [
+            {
+              label: "Akselerometer 1",
+              data: [],
+              fill: false,
+              borderColor: "#FF5F6D",
+              borderWidth: 1,
+              lineTension: 0,
+              pointStyle: "cross"
+            },
+            {
+              label: "Akselerometer 2",
+              data: [],
+              fill: false,
+              borderColor: "#FFC371",
+              borderWidth: 1,
+              lineTension: 0,
+              pointStyle: "triangle"
+            }
+          ]
+        },
+        options: {
+          legend: {
+            position: "bottom"
+            // labels: {usePointStyle: true}
+          },
+          tooltips: {
+            position: "nearest",
+            mode: "index"
+          },
+          scales: {
+            xAxes: [
+              {
+                ticks: {
+                  source: "data"
+                },
+                display: true,
+                stacked: true
+              }
+            ],
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: "G"
+                },
+                display: true,
+                stacked: true
+              }
+            ]
+          }
+        }
+      });
+    },
+    streamData() {
+      const n = 8; // pisah setiap 8 karakter
+      let waktu_ms = 0; // satuan milli second
+
+      this.client.on("connect", () => {
+        this.drawTheChart();
+      });
+
+      this.client.on("packetreceive", async topic => {
+        try {
+          await this.client.subscribe("BAMS");
+          if (topic.payload) {
+            this.loading = false;
+            const node = topic.payload.toString().slice(0, 3);
+            const timestamp =
+              parseInt(topic.payload.toString().slice(3, 11), 16) * 1000;
+            const raw_sensor = topic.payload
+              .toString()
+              .slice(11, topic.payload.toString().length);
+
+            let acc1 = [];
+            let acc2 = [];
+            let acc3 = [];
+            let ane1 = null;
+            let ane2 = null;
+            let ane3 = null;
+
+            for (let index = 11; index <= raw_sensor.length + n; index += n) {
+              const sensor_item_raw = topic.payload
+                .toString()
+                .slice(index, index + n);
+              const sensor_item = Buffer.from(
+                sensor_item_raw,
+                "hex"
+              ).readFloatBE(0);
+
+              if (node == "sb2") {
+                if (index >= 11 && index <= 803) {
+                  acc1.push(sensor_item);
+                } else if (index >= 811 && index <= 1603) {
+                  acc2.push(sensor_item);
+                } else if (index >= 1611 && index <= 2403) {
+                  acc3.push(sensor_item);
+                } else if (index == 2411) {
+                  ane1 = sensor_item;
+                } else if (index == 2419) {
+                  ane2 = sensor_item;
+                } else if (index == 2427) {
+                  ane3 = sensor_item;
+                }
+              } else if (node == "sb1") {
+                if (index >= 11 && index <= 803) {
+                  acc1.push(sensor_item);
+                } else if (index >= 811 && index <= 1603) {
+                  acc2.push(sensor_item);
+                } else if (index == 1611) {
+                  ane1 = sensor_item;
+                } else if (index == 1619) {
+                  ane2 = sensor_item;
+                } else if (index == 1627) {
+                  ane3 = sensor_item;
+                }
+              } else {
+                if (index >= 11 && index <= 803) {
+                  acc1.push(sensor_item);
+                } else if (index >= 811 && index <= 1603) {
+                  acc2.push(sensor_item);
+                }
+              }
+            }
+
+            for (let i = 0; i < 100; i++) {
+              if (this.mChart.data.labels.length > this.maxData)
+                this.mChart.data.labels.shift();
+              let waktu_lokal = new Date(timestamp + waktu_ms);
+              this.mChart.data.labels.push(waktu_lokal.toLocaleString());
+
+              this.mChart.data.datasets.forEach(dataset => {
+                if (dataset.data.length > this.maxData) {
+                  dataset.data.shift();
+                }
+
+                if (dataset.label == "Akselerometer 1") {
+                  dataset.data.push(acc1[i]);
+                } else {
+                  dataset.data.push(acc2[i]);
+                }
+              });
+
+              this.mChart.update();
+
+              waktu_ms += 10; // dalam satuan ms, 1 s ada 100 data getaran
+            }
+          }
+        } catch (error) {
+          console.log(error);
+          this.loading = true;
+        }
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
