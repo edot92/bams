fba565f48c7ec743fcceb38a92a72b8254c683a8
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-18 23:02:30 +0700

    melakukan penyederhanaan page index

--- ui/src/pages/Index.vue
+++ ui/src/pages/Index.vue
@@ -1,22 +1,10 @@
 <template>
-  <q-page padding>
-    <div class="row">
-      <div class="col-1">
-        <img src="~assets/suramadu.png" style="height:130vw;max-height:650px;" />
-      </div>
-      <div class="col-11">
-        <q-card>
-          <q-bar class="bg-secondary">
-            <q-icon name="eva-bulb" color="white" />
-            <div class="text-white">Data Sensor {{node}}</div>
-          </q-bar>
-
-          <q-card-section>
-            <CustomChart :connection="client" :node-sensor="node" />
-          </q-card-section>
-        </q-card>
-      </div>
-    </div>
+  <q-page>
+    <q-card>
+      <q-card-section>
+        <CustomChart />
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
@@ -24,8 +12,6 @@
 </style>
 
 <script>
-import MQTT from "mqtt";
-
 import CustomChart from "../components/Chart";
 
 export default {
@@ -34,17 +20,7 @@ export default {
     CustomChart
   },
   data() {
-    return {
-      client: MQTT.connect("ws://bbta3.bppt.go.id:9623/mqtt", {
-        username: process.env.BAMS_USER,
-        password: process.env.BAMS_PWD
-      }),
-      node : 'sb1'
-    };
-  },
-  destroyed() {
-    this.client.end();
-  },
-  methods: {}
-};
+    return {}
+  }
+}
 </script>
