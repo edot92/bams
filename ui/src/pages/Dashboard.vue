d302778b660a1403b551f3fb763263bcc0dcfc72
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-22 11:37:38 +0700

    membuat halaman awal untuk dashboard

---
+++ ui/src/pages/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding id="dashboard">
+    INI HALAMAN DASHBOARD
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'HalamanDashboard'
+}
+</script>
+
+<style lang="stylus">
+  #dashboard {
+    background-image url('~assets/suramadu.jpg')
+    background-size cover
+  }
+  .q-item .q-router-link--active, .q-item--active {
+    color black
+  }
+</style>
