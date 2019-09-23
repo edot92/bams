2b99162483c0694d3cd2a193ca0c2272eea30756
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-23 22:03:51 +0700

    penyederhanaan state getter

--- ui/src/store/node/getters.js
+++ ui/src/store/node/getters.js
@@ -1,11 +1,3 @@
 export function nodeGetter (state) {
-    return state.number
-}
-
-export function nodeClientGetter (state) {
-    return state.client
-}
-
-export function nodeAneGetter (state) {
-    return state.ane
+    return state
 }
