edd24b4147677ec7f9c6b6973f140ac38c3d0ec0
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-23 22:04:35 +0700

    penggunaan state variable untuk masing-masing sensor node

--- ui/src/store/node/state.js
+++ ui/src/store/node/state.js
@@ -1,5 +1,8 @@
 export default {
-  number: 1,
-  client: null,
-  ane: null
+  sb1_acc1: 0, sb1_acc2: 0, sb1_ane1: 0, sb1_ane2: 0, sb1_ane3: 0,
+  sb2_acc1: 0, sb2_acc2: 0, sb2_acc3: 0, sb2_ane1: 0, sb2_ane2: 0, sb2_ane3: 0,
+  sb3_acc1: 0, sb3_acc2: 0, 
+  sb4_acc1: 0, sb4_acc2: 0,
+  sb5_acc1: 0, sb5_acc2: 0,
+  sb6_acc1: 0, sb6_acc2: 0,
 }
