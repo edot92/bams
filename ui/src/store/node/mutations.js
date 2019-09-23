4faeaac7892cbc0a68a0107d22887d8bcff0c278
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-23 22:04:06 +0700

    penyederhanaan state mutation payload

--- ui/src/store/node/mutations.js
+++ ui/src/store/node/mutations.js
@@ -1,5 +1,3 @@
 export function nodeMutation (state, payload) {
-    state.number = payload.number
-    if ('client' in payload) state.client = payload.client
-    if ('ane' in payload) state.ane = payload.ane
+    state[payload.node] = payload.value
 }
