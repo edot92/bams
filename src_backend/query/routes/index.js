1a36354a2408d02152f741813f2bb41e7d98c3a4
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-26 15:48:09 +0700

    membuat query couchbase dengan url paramater

--- src_backend/query/routes/index.js
+++ src_backend/query/routes/index.js
@@ -6,14 +6,22 @@ const database = require('../helpers/database')
 /* GET home page. */
 router.get('/', (req, res) => {
   let node = req.query.node ? req.query.node : 'sb1'
+  let sekarang = Math.floor(new Date().getTime()/1000.0) * 1000  // dalam ms
+
+  if (req.query.tanggal_waktu) {
+    const pilihan_waktu = new Date(req.query.tanggal_waktu)
+    sekarang = pilihan_waktu.getTime()/1000.0
+  }
+
+  const durasi = 8.64e+7  // dalam ms
+  const kemarin = sekarang - durasi  // dalam ms
 
   database(`SELECT * 
             FROM bbta3_bams_suramadu_test 
-            WHERE node='${node}' 
+            WHERE node='${node}' AND META().id BETWEEN ${kemarin.toString()} AND ${sekarang.toString()}
             LIMIT 200`
   )
 
-  console.log(node, Date.now() * 1000)
   res.status(200).json({
     status: 'STREAMING...'
   })
