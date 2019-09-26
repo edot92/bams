7d7c8e08dd95c3ecceab18e154d19794307d8558
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-09-26 09:53:15 +0700

    membuat root route query database

--- src_backend/query/routes/index.js
+++ src_backend/query/routes/index.js
@@ -1,11 +1,22 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+
+const database = require('../helpers/database')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.json({
-    status: 'OKE'
-  });
-});
+router.get('/', (req, res) => {
+  let node = req.query.node ? req.query.node : 'sb1'
 
-module.exports = router;
+  database(`SELECT * 
+            FROM bbta3_bams_suramadu_test 
+            WHERE node='${node}' 
+            LIMIT 200`
+  )
+
+  console.log(node, Date.now() * 1000)
+  res.status(200).json({
+    status: 'STREAMING...'
+  })
+})
+
+module.exports = router
