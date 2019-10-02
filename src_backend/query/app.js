abf77d68809114c454dc0f0d354c57ed6f5d7a4c
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-10-02 11:26:55 +0700

    menerapkan middleware untuk node app

--- src_backend/query/app.js
+++ src_backend/query/app.js
@@ -1,11 +1,13 @@
 var express = require('express')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+var cors = require('cors')
 
 var indexRouter = require('./routes/index')
 
 var app = express()
 
+app.use(cors())
 app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
