bbe47733bd1d1cf75481ad51784be826dae8b461
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-10-02 14:56:19 +0700

    perbaikan pembulatan grup kecepatan

--- src/helpers/grup_kecepatan.js
+++ src/helpers/grup_kecepatan.js
@@ -2,5 +2,5 @@ exports.grup_kecepatan = (kecepatan) => {
     if (kecepatan == null) return null
     else if (parseFloat(kecepatan) <= 0) return 0
     else if (parseFloat(kecepatan) < 0.5) return 0.5
-    else if(parseFloat(kecepatan) > 0.5) return parseFloat(kecepatan) + 0.5
+    else if(parseFloat(kecepatan) > 0.5) return parseFloat(kecepatan.toFixed(1)) + 0.5
 }
