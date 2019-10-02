7a078173c3a02f1cd7a147eac8e73d0d27e45a4e
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-10-02 11:27:17 +0700

    pengaturan awal integrasi mqtt dan axios

--- ui/src/pages/Query.vue
+++ ui/src/pages/Query.vue
@@ -63,6 +63,7 @@
 
 <script>
 import { date } from 'quasar'
+import MQTT from "mqtt"
 
 export default {
   name: "HalamanQuery",
@@ -70,6 +71,7 @@ export default {
     return {
       date: date.formatDate(new Date(), 'YYYY/MM/DD'),
       node: 'sb1',
+      uid: '',
       opsiNode: ['sb1', 'sb2', 'sb3', 'sb4', 'sb5', 'sb6'],
       queryElement: null,
       trace: {},
@@ -94,50 +96,77 @@ export default {
         displaylogo: false,
         responsive: true,
         displayModeBar: false
-      }
+      },
+      mqtt: MQTT.connect("ws://bbta3.bppt.go.id:9623/mqtt", {
+        username: process.env.BAMS_USER,
+        password: process.env.BAMS_PWD
+      }),
     };
   },
   mounted() {
-    this.queryElement = document.getElementById('query')
-
-    this.trace.trace1 = {
-      x: [1, 2, 3, 4],
-      y: [0, 2, 3, 5],
-      fill: "tozeroy",
-      type: 'scattergl',
-      name: 'acc1',
-      stackgroup: 'acc'
-    }
-
-    this.trace.trace2 = {
-      x: [1, 2, 3, 4],
-      y: [3, 5, 1, 7],
-      fill: "tozeroy",
-      type: 'scattergl',
-      name: 'acc2',
-      stackgroup: 'acc'
-    }
-
-    this.trace.trace3 = {
-      x: [1, 2, 3, 4],
-      y: [10, 7, 3, 1],
-      fill: "tozeroy",
-      type: 'scattergl',
-      name: 'acc3',
-      stackgroup: 'acc'
-    }
-
-    Plotly.newPlot(
-      this.queryElement, 
-      [
-        this.trace.trace1, 
-        this.trace.trace2, 
-        this.trace.trace3
-      ],
-      this.layout,
-      this.conf
-    )
+    this.streamChart()
   },
-  methods: {}
+  destroyed() {
+    this.mqtt.end()
+  },
+  methods: {
+    async streamChart() {
+      this.queryElement = document.getElementById('query')
+      const payload = await this.$axios.get(`http://localhost:9624/?node=${this.node}&?tanggal_waktu=${this.date}`)
+      this.uid = payload.data.id
+
+      this.trace.trace1 = {
+        x: [1, 2, 3, 4],
+        y: [0, 2, 3, 5],
+        fill: "tozeroy",
+        type: 'scattergl',
+        name: 'acc1',
+        mode: 'lines',
+        stackgroup: 'acc'
+      }
+
+      this.trace.trace2 = {
+        x: [1, 2, 3, 4],
+        y: [3, 5, 1, 7],
+        fill: "tozeroy",
+        type: 'scattergl',
+        name: 'acc2',
+        mode: 'lines',
+        stackgroup: 'acc'
+      }
+
+      this.trace.trace3 = {
+        x: [1, 2, 3, 4],
+        y: [10, 7, 3, 1],
+        fill: "tozeroy",
+        type: 'scattergl',
+        name: 'acc3',
+        mode: 'lines',
+        stackgroup: 'acc'
+      }
+
+      this.mqtt.on("packetreceive", async (topic) => {
+        try {
+          await this.mqtt.subscribe(`BAMS/query/${this.uid}`)
+
+          if (topic.payload) console.log(new TextDecoder("utf-8").decode(topic.payload))
+        } catch (error) {
+          console.log(error.message)
+          this.mqtt.end()
+        }
+      })
+
+      Plotly.plot(
+        this.queryElement, 
+        [
+          this.trace.trace1, 
+          this.trace.trace2, 
+          this.trace.trace3
+        ],
+        this.layout,
+        this.conf
+      )
+    }
+  }
 };
 </script>
