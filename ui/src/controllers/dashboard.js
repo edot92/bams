579f02371ec4782d0764f29a8146f4b6479de240
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-10-04 16:13:30 +0700

    penyesuaianstate mutation untuk live data sensor

--- ui/src/controllers/dashboard.js
+++ ui/src/controllers/dashboard.js
@@ -1,59 +1,31 @@
-export const processData = (topic, split_character, store) => {
-  if (topic.payload) {
-    const node = topic.payload.toString().slice(0, 3)
-    const raw_sensor = topic.payload.toString().slice(11, topic.payload.toString().length)
-
-    // let sensor = store.getters['node/nodeGetter']
-
-    for (let index = 11; index <= raw_sensor.length + split_character; index += split_character) {
-      const sensor_item_raw = topic.payload
-        .toString()
-        .slice(index, index + split_character)
-      const sensor_item = Buffer.from(
-        sensor_item_raw,
-        "hex"
-      ).readFloatBE(0).toFixed(5)
-
-      if (node == "sb2") {
-        if (index >= 11 && index <= 803) {
-          store.commit('node/nodeMutation', {node: 'sb2_acc1', value: sensor_item})
-        } else if (index >= 811 && index <= 1603) {
-          store.commit('node/nodeMutation', {node: 'sb2_acc2', value: sensor_item})
-        } else if (index >= 1611 && index <= 2403) {
-          store.commit('node/nodeMutation', {node: 'sb2_acc3', value: sensor_item})
-        } else if (index == 2411) {
-          store.commit('node/nodeMutation', {node: 'sb2_ane1', value: sensor_item})
-        } else if (index == 2419) {
-          store.commit('node/nodeMutation', {node: 'sb2_ane2', value: toCompass(sensor_item)})
-        } else if (index == 2427) {
-          store.commit('node/nodeMutation', {node: 'sb2_ane3', value: sensor_item})
-        }
-      } else if (node == "sb1") {
-        if (index >= 11 && index <= 803) {
-          store.commit('node/nodeMutation', {node: 'sb1_acc1', value: sensor_item})
-        } else if (index >= 811 && index <= 1603) {
-          store.commit('node/nodeMutation', {node: 'sb1_acc2', value: sensor_item})
-        } else if (index == 1611) {
-          store.commit('node/nodeMutation', {node: 'sb1_ane1', value: sensor_item})
-        } else if (index == 1619) {
-          store.commit('node/nodeMutation', {node: 'sb1_ane2', value: toCompass(sensor_item)})
-        } else if (index == 1627) {
-          store.commit('node/nodeMutation', {node: 'sb1_ane3', value: sensor_item})
-        }
-      } else {
-        if (index >= 11 && index <= 803) {
-          if (node == 'sb3') {store.commit('node/nodeMutation', {node: 'sb3_acc1', value: sensor_item})}
-          else if (node == 'sb4') {store.commit('node/nodeMutation', {node: 'sb4_acc1', value: sensor_item})}
-          else if (node == 'sb5') {store.commit('node/nodeMutation', {node: 'sb5_acc1', value: sensor_item})}
-          else if (node == 'sb6') {store.commit('node/nodeMutation', {node: 'sb6_acc1', value: sensor_item})}
-        } else if (index >= 811 && index <= 1603) {
-          if (node == 'sb3') {store.commit('node/nodeMutation', {node: 'sb3_acc2', value: sensor_item})}
-          else if (node == 'sb4') {store.commit('node/nodeMutation', {node: 'sb4_acc2', value: sensor_item})}
-          else if (node == 'sb5') {store.commit('node/nodeMutation', {node: 'sb5_acc2', value: sensor_item})}
-          else if (node == 'sb6') {store.commit('node/nodeMutation', {node: 'sb6_acc2', value: sensor_item})}
-        }
-      }
-    }
+export const processData = (payload, store) => {
+  if (payload.node == "sb2") {
+    store.commit('node/nodeMutation', {node: 'sb2_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb2_acc2', value: payload.acc2.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb2_acc3', value: payload.acc3.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb2_ane1', value: payload.kecepatan.toFixed(1)})
+    store.commit('node/nodeMutation', {node: 'sb2_ane2', value: payload.kompas})
+  } else if (payload.node == "sb1") {
+    store.commit('node/nodeMutation', {node: 'sb1_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb1_acc2', value: payload.acc2.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb1_ane1', value: payload.kecepatan.toFixed(1)})
+    store.commit('node/nodeMutation', {node: 'sb1_ane2', value: payload.kompas})
+  }
+  else if (payload.node == 'sb3') {
+    store.commit('node/nodeMutation', {node: 'sb3_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb3_acc2', value: payload.acc2.toFixed(4)})
+  }
+  else if (payload.node == 'sb4') {
+    store.commit('node/nodeMutation', {node: 'sb4_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb4_acc2', value: payload.acc2.toFixed(4)})
+  }
+  else if (payload.node == 'sb5') {
+    store.commit('node/nodeMutation', {node: 'sb5_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb5_acc2', value: payload.acc2.toFixed(4)})
+  }
+  else if (payload.node == 'sb6') {
+    store.commit('node/nodeMutation', {node: 'sb6_acc1', value: payload.acc1.toFixed(4)})
+    store.commit('node/nodeMutation', {node: 'sb6_acc2', value: payload.acc2.toFixed(4)})
   }
 }
 
