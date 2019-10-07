5f641452276d9516ce01e1482663d051e0ebe809
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-10-07 14:30:36 +0700

    ganti url mqtt

--- src_backend/query/helpers/client.js
+++ src_backend/query/helpers/client.js
@@ -1,5 +1,5 @@
 const mqtt = require('mqtt')
-const client  = mqtt.connect("mqtt://10.10.42.17:9621/mqtt", {
+const client  = mqtt.connect("ws://bbta3.bppt.go.id:9623/mqtt", {
     username: process.env.BAMS_USER,
     password: process.env.BAMS_PWD
 })
