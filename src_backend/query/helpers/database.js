305c1991d839956137b9e4d7972473954e9e8467
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-11-06 09:36:01 +0700

    memperbaiki masalah konkesi bucket

--- src_backend/query/helpers/database.js
+++ src_backend/query/helpers/database.js
@@ -4,10 +4,10 @@ const mqtt_client = require('../helpers/client')
 const cluster = new couchbase.Cluster('couchbase://10.10.42.16/?n1ql_timeout=600.0')
 cluster.authenticate(process.env.BAMS_USER, process.env.BAMS_PWD)
 
-const bucket = cluster.openBucket('bbta3_bams_suramadu_test')
 const N1qlQuery = couchbase.N1qlQuery
 
 const openBucket = (query, uid) => {
+    const bucket = cluster.openBucket('bbta3_bams_suramadu_test')
     const req = bucket.query(
         N1qlQuery.fromString(query)
     )
