64d8f9f627bae589cfebfa95f783ce809c49a856
Arizal Akbar Zikri <akbarpn136@gmail.com>
2019-12-21 21:35:26 +0700

    hapus console log

--- ui/src/controllers/dashboard.js
+++ ui/src/controllers/dashboard.js
@@ -1,6 +1,5 @@
 export const processData = (payload, store) => {
   if (payload.node == "sb2") {
-    console.log(payload)
     store.commit('node/nodeMutation', {node: 'sb2_acc1', value: payload.acc1 ? payload.acc1.toFixed(4) : 0})
     store.commit('node/nodeMutation', {node: 'sb2_acc2', value: payload.acc2 ? payload.acc2.toFixed(4) : 0})
     store.commit('node/nodeMutation', {node: 'sb2_acc3', value: payload.acc3 ? payload.acc3.toFixed(4) : 0})
