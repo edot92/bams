2d84e2b15f044b36369e20312674457ec3a0df2a
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-11-17 09:32:07 +0700

    refactoring quasar config untuk quasar cli v2

--- ui/quasar.conf.js
+++ ui/quasar.conf.js
@@ -24,6 +24,7 @@ module.exports = function (ctx) {
 
     framework: {
       // all: true, // --- includes everything; for dev only!
+      all: 'importStrategy',
       cssAddon: true,
 
       components: [
@@ -82,8 +83,6 @@ module.exports = function (ctx) {
       // lang: 'de' // Quasar language
     },
 
-    supportIE: false,
-
     build: {
       scopeHoisting: true,
       // vueRouterMode: 'history',
@@ -121,27 +120,27 @@ module.exports = function (ctx) {
         theme_color: '#027be3',
         icons: [
           {
-            'src': 'statics/icons/icon-128x128.png',
+            'src': 'public/icons/icon-128x128.png',
             'sizes': '128x128',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-192x192.png',
+            'src': 'public/icons/icon-192x192.png',
             'sizes': '192x192',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-256x256.png',
+            'src': 'public/icons/icon-256x256.png',
             'sizes': '256x256',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-384x384.png',
+            'src': 'public/icons/icon-384x384.png',
             'sizes': '384x384',
             'type': 'image/png'
           },
           {
-            'src': 'statics/icons/icon-512x512.png',
+            'src': 'public/icons/icon-512x512.png',
             'sizes': '512x512',
             'type': 'image/png'
           }
