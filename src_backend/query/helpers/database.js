95c8bd19ea74cf645e62a1c388c3f4c2bde93bc9
Arizal Akbar Zikri <akbarpn136@gmail.com>
2020-11-17 19:14:01 +0700

    ganti endpoint dengan env agar fleksibel

--- src_backend/query/helpers/database.js
+++ src_backend/query/helpers/database.js
@@ -1,7 +1,7 @@
 const couchbase = require('couchbase')
 const mqtt_client = require('../helpers/client')
 
-const cluster = new couchbase.Cluster('couchbase://10.10.42.16/?n1ql_timeout=600.0')
+const cluster = new couchbase.Cluster(`couchbase://${process.env.BAMS_HOST_COUCH}/?n1ql_timeout=600.0`)
 cluster.authenticate(process.env.BAMS_USER, process.env.BAMS_PWD)
 
 const N1qlQuery = couchbase.N1qlQuery
